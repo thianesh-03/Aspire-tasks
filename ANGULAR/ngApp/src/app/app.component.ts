f2671e2af3fbb6709e4e75ea3566d4be3922316e
thianesh-03 <thianesh2003@gmail.com>
2024-05-14 23:18:55 +0530

    angular upto event binding

--- ANGULAR/ngApp/src/app/app.component.ts
+++ ANGULAR/ngApp/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngApp';
+  showTitle(){
+    return "Thianesh";
+  }
+  clickCount = 0;
+  addCount(){
+    this.clickCount++;
+  }
+  reduceCount(){
+    this.clickCount--;
+  }
 }
