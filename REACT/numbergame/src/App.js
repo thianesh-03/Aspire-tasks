52ddc80732a57e897aa80a5ebeab041b033d8c65
thianesh-03 <thianesh2003@gmail.com>
2024-07-30 23:20:02 +0530

    game completed

--- REACT/numbergame/src/App.js
+++ REACT/numbergame/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import Result from './Result';
+const correctValue = Math.floor(Math.random()*10) + 1;
 
 function App() {
+  const [term,setTerm] = useState();
+
+  function handleChange(event){
+    setTerm(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="head">
+        <label>Guess a number between 1 - 10 (answer:{correctValue})</label>
+      </div>
+      <input id="term" name="term" type="text" onChange={handleChange}/>
+      <Result correctValue={correctValue} term={term}/>
     </div>
   );
 }
