fdceaba1957fbc6bd78c76b7fd477d471014db83
thianesh-03 <thianesh2003@gmail.com>
2024-08-07 12:44:48 +0530

    react crud updates

--- REACT/react-crud/src/App.js
+++ REACT/react-crud/src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import { Button, EditableText } from "@blueprintjs/core";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+  }, []);
+
   return (
     <div className="App">
-      
+      <table className="bp4-html-table modifier">
+        <thead>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Website</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td><EditableText value={user.email}/></td>
+              <td><EditableText value={user.website}/></td>
+              <td>
+                <Button intent="primary">Update</Button>{" "}
+                <Button intent="danger">Delete</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
